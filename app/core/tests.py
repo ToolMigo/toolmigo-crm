cadf1e7e76d5a4158f05b94690915e95c3ec1ff0
ToolMigo <info@toolmigo.nl>
2026-09-06 20:09:13 +0200

    Markeer Taken niet actief op dashboard

--- app/core/tests.py
+++ app/core/tests.py
@@ -18,6 +18,9 @@ class TenantSecurityTests(TestCase):
         Membership.objects.create(organization=self.a,user=self.worker,role=Membership.Role.EMPLOYEE)
         self.foreign_member=Membership.objects.create(organization=self.b,user=self.foreign,role=Membership.Role.OWNER)
     def test_login_required(self): self.assertRedirects(self.client.get('/'),'/login/?next=/')
+    def test_dashboard_marks_only_dashboard_navigation_active(self):
+        self.client.force_login(self.owner); response=self.client.get(reverse('dashboard'))
+        self.assertEqual(response.content.count(b'class="active"'),1)
     def test_favicon_uses_toolmigo_logo(self): self.assertRedirects(self.client.get('/favicon.ico'),'/static/img/toolmigo-logo.jpeg',status_code=301,fetch_redirect_response=False)
     def test_employee_cannot_open_team_management(self):
         self.client.force_login(self.worker); self.assertRedirects(self.client.get(reverse('team')),reverse('dashboard'))
